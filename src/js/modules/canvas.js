a4563abb07c1585ef65bbd92defb4efdf6a2ac71
sammorrisdesign <mail@sammorrisdesign.co.uk>
2017-11-20 17:32:56 -0500

    performance

--- src/js/modules/canvas.js
+++ src/js/modules/canvas.js
@@ -1,11 +1,10 @@
 var $ = require('../vendor/jquery');
 
-var canvas, W, H, interval, ctx,
-    tmpCanvas,
-    tmpCtx,
+var canvas, W, H, ctx,
     circles = [],
     circleCount = 40,
-    degrees = 0;
+    degrees = 0,
+    pi2 = Math.PI * 2;
 
 module.exports = {
     init: function() {
@@ -20,19 +19,12 @@ module.exports = {
     },
 
     createCanvas: function() {
-        // real canvas
         canvas = document.getElementsByClassName('vortex__canvas')[0];
         this.setCanvasSize();
         ctx = canvas.getContext('2d');
 
-        // temporary canvas used for calculating position
-        tmpCanvas = document.createElement('canvas');
-        tmpCanvas.width = W;
-        tmpCanvas.height = H;
-        tmpCtx = tmpCanvas.getContext('2d');
-
         this.generateCircles();
-        this.startDrawing();
+        this.draw();
     },
 
     setCanvasSize: function() {
@@ -48,45 +40,45 @@ module.exports = {
             var y = H / 2 + Math.floor(Math.random()*40) -20;
 
             circles[i] = {
-                x: x,
-                y: y,
                 offset: Math.sqrt( ((W/2 - x) * (W/2 - x)) + ((H/2 - y) * (H/2 - y)) ),
                 angle: Math.floor(Math.random()*360) + 1,
-                speed: Math.floor(Math.random()*8),
+                speed: Math.floor(Math.random()*8) + 4,
+                radius: 400 + (i * 18),
+                rgb: Math.floor(34 + (i * (221 / circleCount))),
                 cx: 0,
                 cy: 0
             }
         }
-        console.log(circles);
+
+        this.updateCircles();
     },
 
-    startDrawing: function() {
-        interval = setInterval(function() {
-            this.draw();
-        }.bind(this), 40)
-    },
+    updateCircles: function() {
+        for (var i = 0; i < circleCount; i++) {
+            var circle = circles[i];
 
-    updateCircle: function(i) {
-        var circle = circles[i];
+            circle.angle += circle.speed;
+            circle.angle = circle.angle > 360 ? 0 : circle.angle;
+            circle.cx = (W / 2) + circle.offset * Math.cos(circle.angle  * (Math.PI / 180));
+            circle.cy = (H / 2) + circle.offset * Math.sin(circle.angle  * (Math.PI / 180));
 
-        circle.angle += circle.speed;
-        circle.cx = (W / 2) + circle.offset * Math.cos(circle.angle  * (Math.PI / 180));
-        circle.cy = (H / 2) + circle.offset * Math.sin(circle.angle  * (Math.PI / 180));
+            circles[i] = circle;
+        }
 
-        circles[i] = circle;
-        return circles[i];
+        this.draw();
+        requestAnimationFrame(this.updateCircles.bind(this));
     },
 
     draw: function() {
         ctx.clearRect(0, 0, W, H);
+        ctx.lineWidth = 6;
 
         for (var i = 0; i < circleCount; i++) {
-            var circle = this.updateCircle(i);
-
+            var circle = circles[i];
             ctx.beginPath();
-            ctx.ellipse(circle.cx, circle.cy, i*18 + 400, i*18 + 400, 0, 0, 2 * Math.PI);
-            ctx.strokeStyle = 'rgba(255, 255, 255,' + 1 + ')';
-            ctx.lineWidth = 10;
+            ctx.ellipse(circle.cx, circle.cy, circle.radius, circle.radius, 0, 0, 2 * Math.PI);
+            ctx.strokeStyle = 'rgb(' + circle.rgb + ',' + circle.rgb + ',' + circle.rgb + ')';
+            ctx.closePath();
             ctx.stroke();
         }
     }
